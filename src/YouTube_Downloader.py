e67923933d39c76a92bff7ef31b557677658f552
Ale2328 <78653548+Ale2328@users.noreply.github.com>
2023-01-10 19:15:04 +0100

    Exe_updated

--- src/YouTube_Downloader.py
+++ src/YouTube_Downloader.py
@@ -11,7 +11,7 @@ from pytube import YouTube
 def download_video():
     try:
         
-        directory = "./video"
+        directory = "../video"
         url = textbox.get()
         yt = YouTube(url)
         yt.streams.get_highest_resolution().download(directory)
@@ -26,13 +26,13 @@ def download_video():
             textbox.delete(0, 'end')
 
 root = tk.Tk()
-root.tk.call('wm', 'iconphoto', root._w, tk.PhotoImage(file='icon.png'))
+root.tk.call('wm', 'iconphoto', root._w, tk.PhotoImage(file='./icon.png'))
 root.title("YouTube Downloader")
 root.geometry("500x500")
 root.resizable(False, False)
 
 frame = tk.Frame(root).grid(row=0, column=0, sticky="nw")
-background = ImageTk.PhotoImage(file="bg.png")
+background = ImageTk.PhotoImage(file="./bg.png")
 
 global widget_background
 widget_background = tk.Label(frame, image=background)
@@ -42,7 +42,7 @@ widget_background.grid(row=0, column=0, sticky="nw")
 textbox = tk.Entry(frame, width=25, font=("Arial", 15), bd=0)
 textbox.place(x=110, y=310)
 
-btn = ImageTk.PhotoImage(file="btn.png")
+btn = ImageTk.PhotoImage(file="./btn.png")
 widget_btn = tk.Button(frame, image=btn, command=download_video, bg="red")
 widget_btn.image = btn
 widget_btn.place(x=150, y=380)
