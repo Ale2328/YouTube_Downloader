cc4fa23b409f545f2ad7152fe8f68eec28a8c8a3
Ale2328 <78653548+Ale2328@users.noreply.github.com>
2023-01-11 22:17:39 +0100

    Add comments

--- src/YouTube_Downloader.py
+++ src/YouTube_Downloader.py
@@ -1,3 +1,12 @@
+#----------------------------------#
+#       YOUTUBE DOWNLOADER         #
+#----------------------------------#
+#   Author: Alessandro Di Bernardo #
+#   GitHub: Ale2328                #
+#   Date: 11/01/2023               #
+#----------------------------------#
+                                 
+#IMPORTS
 from cgitb import text
 from msilib.schema import Directory
 from re import X
@@ -9,23 +18,27 @@ from turtle import color
 from PIL import ImageTk
 from pytube import YouTube
 
+#CONTROL_FORMAT
 def control_format():
     if format_combobox.get() == 'mp3':
         download_audio()
     else:
         download_video()
 
+#DOWNLOAD_VIDEO
 def download_video():
-    print("mp4")
+
+    #Choose directory
     directory = filedialog.askdirectory(initialdir = "/",title = "Where you want to save?")
     
     try:
-        
-
+        #Get input from textbox
         url = textbox.get()
         yt = YouTube(url)
+
+        #Download video
         yt.streams.get_highest_resolution().download(directory)
-        
+    
         messagebox.showinfo("Success", "Video Downloaded Successfully")
 
         if textbox.get() != None:
@@ -37,14 +50,18 @@ def download_video():
             textbox.delete(0, 'end')
 
 
+#DOWNLOAD_AUDIO
 def download_audio():
-    
+
+    #Choose directory
     directory = filedialog.askdirectory(initialdir = "/",title = "Where you want to save?")
 
     try:
-        
+        #Get input from textbox
         url = textbox.get()
         yt = YouTube(url)
+
+        #Download audio 
         video = yt.streams.filter(only_audio=True).first()
         video.download(directory)
         
@@ -58,12 +75,7 @@ def download_audio():
         if textbox.get() != None:
             textbox.delete(0, 'end')
 
-    
-
-
-
-
-
+#WINDOW INITIALIZATION
 root = tk.Tk()
 root.tk.call('wm', 'iconphoto', root._w, tk.PhotoImage(file='./icon.png'))
 root.title("YouTube Downloader")
@@ -73,28 +85,26 @@ root.resizable(False, False)
 frame = tk.Frame(root).grid(row=0, column=0, sticky="nw")
 background = ImageTk.PhotoImage(file="./bg.png")
 
+#BACKGROUND
 global widget_background
 widget_background = tk.Label(frame, image=background)
 widget_background.image = background
 widget_background.grid(row=0, column=0, sticky="nw")
 
+#TEXTBOX
 textbox = tk.Entry(frame, width=25, font=("Arial", 15), bd=0)
 textbox.place(x=110, y=310)
 
-btn = ImageTk.PhotoImage(file="./btn.png")
-
+#COMBOBOX
 format_selected = StringVar()
 format_combobox = ttk.Combobox(root, textvariable=format_selected)
-
 format_combobox.grid(row=0, column=0,sticky="nw")
 format_combobox.place(x=180,y=340)
 format_combobox['values'] = ["mp4","mp3"]
 format_combobox['state'] = 'readonly'
 
-
-
-
-
+#DOWNLOAD_BUTTON
+btn = ImageTk.PhotoImage(file="./btn.png")
 widget_btn = tk.Button(frame, image=btn, command=control_format, bg="red")
 widget_btn.image = btn
 widget_btn.place(x=150, y=380)
