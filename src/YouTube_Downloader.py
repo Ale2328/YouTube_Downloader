ec36dbd8e888ee23e995aa5cc1f1a0c78df59550
Ale2328 <78653548+Ale2328@users.noreply.github.com>
2023-01-11 21:47:29 +0100

    Now u can convert your file into mp3 or mp4

--- src/YouTube_Downloader.py
+++ src/YouTube_Downloader.py
@@ -1,6 +1,7 @@
 from cgitb import text
 from msilib.schema import Directory
 from re import X
+from tkinter import StringVar, ttk
 import tkinter as tk
 from tkinter.font import BOLD
 from tkinter import filedialog, messagebox
@@ -8,13 +9,23 @@ from turtle import color
 from PIL import ImageTk
 from pytube import YouTube
 
+def control_format():
+    if format_combobox.get() == 'mp3':
+        download_audio()
+    else:
+        download_video()
+
 def download_video():
+    print("mp4")
+    directory = filedialog.askdirectory(initialdir = "/",title = "Where you want to save?")
+    
     try:
         
-        directory = "./video"
+
         url = textbox.get()
         yt = YouTube(url)
         yt.streams.get_highest_resolution().download(directory)
+        
         messagebox.showinfo("Success", "Video Downloaded Successfully")
 
         if textbox.get() != None:
@@ -24,6 +35,34 @@ def download_video():
         messagebox.showerror("Error", "Invalid URL")
         if textbox.get() != None:
             textbox.delete(0, 'end')
+
+
+def download_audio():
+    
+    directory = filedialog.askdirectory(initialdir = "/",title = "Where you want to save?")
+
+    try:
+        
+        url = textbox.get()
+        yt = YouTube(url)
+        video = yt.streams.filter(only_audio=True).first()
+        video.download(directory)
+        
+        messagebox.showinfo("Success", "Audio Downloaded Successfully")
+
+        if textbox.get() != None:
+            textbox.delete(0, 'end')
+        
+    except:
+        messagebox.showerror("Error", "Invalid URL")
+        if textbox.get() != None:
+            textbox.delete(0, 'end')
+
+    
+
+
+
+
 
 root = tk.Tk()
 root.tk.call('wm', 'iconphoto', root._w, tk.PhotoImage(file='./icon.png'))
@@ -43,7 +82,20 @@ textbox = tk.Entry(frame, width=25, font=("Arial", 15), bd=0)
 textbox.place(x=110, y=310)
 
 btn = ImageTk.PhotoImage(file="./btn.png")
-widget_btn = tk.Button(frame, image=btn, command=download_video, bg="red")
+
+format_selected = StringVar()
+format_combobox = ttk.Combobox(root, textvariable=format_selected)
+
+format_combobox.grid(row=0, column=0,sticky="nw")
+format_combobox.place(x=180,y=340)
+format_combobox['values'] = ["mp4","mp3"]
+format_combobox['state'] = 'readonly'
+
+
+
+
+
+widget_btn = tk.Button(frame, image=btn, command=control_format, bg="red")
 widget_btn.image = btn
 widget_btn.place(x=150, y=380)
 
